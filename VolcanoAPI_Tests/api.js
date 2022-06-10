c78a4372eb854d68c835350807cbeceb0076cc58
Zac Adams <zachery.adams@ivolve.com>
2022-06-10 19:58:48 +1000

    Update tests to remote

--- VolcanoAPI_Tests/api.js
+++ VolcanoAPI_Tests/api.js
@@ -1,5 +1,5 @@
 //If you are serving your server on any port other than 3001, change the port here, or alternatively change the url to approriate
-// const REMOTE_API_URL = `https://vdi-vl24-171.qut.edu.au`;
-const REMOTE_API_URL = `http://localhost:3000`;
+const REMOTE_API_URL = `https://vdi-vl24-171.qut.edu.au`;
+// const REMOTE_API_URL = `http://localhost:3000`;
 
 module.exports = REMOTE_API_URL;
